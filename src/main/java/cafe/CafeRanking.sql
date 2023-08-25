0780d794dfc410b2219d1442b95c7c67f5a14e87
eh44y <96423054+eh44y@users.noreply.github.com>
2023-08-25 09:56:40 +0900

    Update CafeRanking.sql

--- src/main/java/cafe/CafeRanking.sql
+++ src/main/java/cafe/CafeRanking.sql
@@ -37,15 +37,15 @@ CREATE TABLE HEART(
 );
 CREATE TABLE review(
     id IDENTITY,
-    cafeId BIGINT NOT NULL REFERENCES cafe(id),
-    customerId BIGINT NOT NULL REFERENCES customer(id),
+    cafeId BIGINT NOT NULL REFERENCES cafe(id) ON DELETE CASCADE,
+    customerId BIGINT NOT NULL REFERENCES customer(id) ON DELETE CASCADE,
     img VARCHAR,
     content VARCHAR NOT NULL,
     date DATE NOT NULL
 );
 CREATE TABLE community(
     id IDENTITY,
-    customerId BIGINT NOT NULL REFERENCES customer(id),
+    customerId BIGINT NOT NULL REFERENCES customer(id) ON DELETE CASCADE,
     title VARCHAR(64) NOT NULL,
     img VARCHAR,
     content VARCHAR NOT NULL,
@@ -55,8 +55,8 @@ CREATE TABLE community(
 );
 CREATE TABLE comment(
     id IDENTITY,
-    customerId BIGINT NOT NULL REFERENCES customer(id),
-    communityId BIGINT NOT NULL REFERENCES community(id),
+    customerId BIGINT NOT NULL REFERENCES customer(id) ON DELETE CASCADE,
+    communityId BIGINT NOT NULL REFERENCES community(id) ON DELETE CASCADE,
     content VARCHAR,
     date TIMESTAMP
 );
